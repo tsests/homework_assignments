41a7fbee74977cd246d2d0203ee148f07be6042a
tsests <134438075+tsests@users.noreply.github.com>
2024-11-15 02:03:45 +0700

    Update test_3_prac.py

--- tests/test_3_prac.py
+++ tests/test_3_prac.py
@@ -15,27 +15,19 @@ from prac1 import fizz_buzz
 #from prac5 import is_contain_three_words_in_a_row
 #from prac6 import jokes
 
-
-def test_fizz_buzz_fizz():
-    with patch('builtins.input', return_value='3'):  # Мокаем input
-        with patch('builtins.print') as mock_print:  # Мокаем print
-            fizz_buzz(abs(int(input())))
-            mock_print.assert_called_with('Fizz')
-
-def test_fizz_buzz_buzz():
-    with patch('builtins.input', return_value='5'):
-        with patch('builtins.print') as mock_print:
-            fizz_buzz(abs(int(input())))
-            mock_print.assert_called_with('Buzz')
-
-def test_fizz_buzz_fizzbuzz():
-    with patch('builtins.input', return_value='15'):
-        with patch('builtins.print') as mock_print:
-            fizz_buzz(abs(int(input())))
-            mock_print.assert_called_with('Fizz Buzz')
-
-def test_fizz_buzz_number():
-    with patch('builtins.input', return_value='7'):
-        with patch('builtins.print') as mock_print:
-            fizz_buzz(abs(int(input())))
-            mock_print.assert_called_with(7)
+@pytest.mark.parametrize(
+    "input_value, expected_output",
+    [
+        ('3', 'Fizz'),
+        ('5', 'Buzz'),
+        ('15', 'Fizz Buzz'),
+        ('7', '7'),
+        ('0', 'Fizz Buzz'),
+        ('45', 'Fizz Buzz')
+    ]
+)
+def test_fizz_buzz(input_value, expected_output):
+    # Мокаем input и print
+    with patch.multiple('builtins', input=lambda: input_value, print=lambda x: x) as mocks:
+        output = fizz_buzz(abs(int(input_value)))
+        mocks['print'].assert_called_with(expected_output)
