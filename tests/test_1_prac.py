b86cecb91b3548d4126cd32365cc39a4aecad41e
tsests <134438075+tsests@users.noreply.github.com>
2024-11-14 20:40:15 +0700

    Update test_1_prac.py

--- tests/test_1_prac.py
+++ tests/test_1_prac.py
@@ -3,24 +3,21 @@ import pytest
 
 # 1. Тест для 1.py (Приветствие)
 def test_1_prac_greeting(capsys):
-    # Запуск скрипта и проверка вывода
-    subprocess.run(["python3", "1_prac/1.py"])
-    captured = capsys.readouterr()
+    subprocess.run(["python3", "1_prac/1.py"])  # Запускаем скрипт 1.py
+    captured = capsys.readouterr()  # Перехватываем вывод
     expected_output = "Hello Ivan Petrov! You just delved into Python. Great start!\n"
-    assert captured.out == expected_output
+    assert captured.out == expected_output  # Проверяем вывод
 
 # 2. Тест для 2.py (Заголовок)
 def test_2_prac_title(capsys):
-    # Запуск скрипта и проверка вывода
-    subprocess.run(["python3", "1_prac/2.py"])
-    captured = capsys.readouterr()
+    subprocess.run(["python3", "1_prac/2.py"])  # Запускаем скрипт 2.py
+    captured = capsys.readouterr()  # Перехватываем вывод
     expected_output = "Hello World\n"
-    assert captured.out == expected_output
+    assert captured.out == expected_output  # Проверяем вывод
 
 # 3. Тест для 3.py (Форматированная сумма)
 def test_3_prac_currency(capsys):
-    # Запуск скрипта и проверка вывода
-    subprocess.run(["python3", "1_prac/3.py"])
-    captured = capsys.readouterr()
+    subprocess.run(["python3", "1_prac/3.py"])  # Запускаем скрипт 3.py
+    captured = capsys.readouterr()  # Перехватываем вывод
     expected_output = "100,500.16\n"
-    assert captured.out == expected_output
+    assert captured.out == expected_output  # Проверяем вывод
