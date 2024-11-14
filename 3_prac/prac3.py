3b87937dd8174ca1fc82168b763f789a22901b5f
tsests <134438075+tsests@users.noreply.github.com>
2024-11-15 02:21:39 +0700

    Update prac3.py

--- 3_prac/prac3.py
+++ 3_prac/prac3.py
@@ -1,12 +1,15 @@
 ### 3. Последовательность
+def main();
+    user_input_int: int = 0
+    while user_input_int < 1:
+        user_input: str = input()
+        user_input_int = int(user_input)
+    
+    for i in range(user_input_int):
+        print(i+1, end = "")
+    
+    # У меня без этого переноса появляеьтся символ терминала в конце строки.
+    print()
 
-user_input_int: int = 0
-while user_input_int < 1:
-    user_input: str = input()
-    user_input_int = int(user_input)
-
-for i in range(user_input_int):
-    print(i+1, end = "")
-
-# У меня без этого переноса появляеьтся символ терминала в конце строки.
-print()
+if __name__ == "__main__":
+    main()
