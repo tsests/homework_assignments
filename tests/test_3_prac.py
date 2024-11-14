26e08866db60cb8cff68164b57fdf27389b3dc69
tsests <134438075+tsests@users.noreply.github.com>
2024-11-15 01:56:26 +0700

    Update test_3_prac.py

--- tests/test_3_prac.py
+++ tests/test_3_prac.py
@@ -15,31 +15,27 @@ from prac4 import get_secret_message
 from prac5 import is_contain_three_words_in_a_row
 from prac6 import jokes
 
+
 def test_fizz_buzz_fizz():
-    # Тестируем, что при вводе 3 функция выведет "Fizz"
-    with patch('builtins.input', return_value='3'):
-        with patch('builtins.print') as mock_print:
+    with patch('builtins.input', return_value='3'):  # Мокаем input
+        with patch('builtins.print') as mock_print:  # Мокаем print
             fizz_buzz(abs(int(input())))
             mock_print.assert_called_with('Fizz')
 
 def test_fizz_buzz_buzz():
-    # Тестируем, что при вводе 5 функция выведет "Buzz"
     with patch('builtins.input', return_value='5'):
         with patch('builtins.print') as mock_print:
             fizz_buzz(abs(int(input())))
             mock_print.assert_called_with('Buzz')
 
 def test_fizz_buzz_fizzbuzz():
-    # Тестируем, что при вводе 15 функция выведет "Fizz Buzz"
     with patch('builtins.input', return_value='15'):
         with patch('builtins.print') as mock_print:
             fizz_buzz(abs(int(input())))
             mock_print.assert_called_with('Fizz Buzz')
 
 def test_fizz_buzz_number():
-    # Тестируем, что при вводе 7 функция выведет "7"
     with patch('builtins.input', return_value='7'):
         with patch('builtins.print') as mock_print:
             fizz_buzz(abs(int(input())))
             mock_print.assert_called_with(7)
-
