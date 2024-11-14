16a41bbb60c73b8e696dd3959c1d438496b61a8f
tsests <134438075+tsests@users.noreply.github.com>
2024-11-15 02:10:24 +0700

    Update test_3_prac.py

--- tests/test_3_prac.py
+++ tests/test_3_prac.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../3
 
 # Импортируем функции
 from prac1 import fizz_buzz
-#from prac2 import estimate_value
+from prac2 import estimate_value
 #from prac3 import generate_sequence
 #from prac4 import get_secret_message
 #from prac5 import is_contain_three_words_in_a_row
@@ -31,3 +31,27 @@ def test_fizz_buzz(input_value, expected_output):
     with patch('builtins.input', return_value=input_value), patch('builtins.print') as mock_print:
         fizz_buzz(abs(int(input_value)))  # Вызываем функцию с преобразованием
         mock_print.assert_called_with(expected_output)
+
+# Тесты для функции estimate_value
+@pytest.mark.parametrize(
+    "input_value, expected_output",
+    [
+        ('1', 'Плохо'),          # Нечетное число
+        ('2', 'Неплохо'),        # Число от 2 до 5
+        ('5', 'Неплохо'),        # Число от 2 до 5
+        ('6', 'Так себе'),       # Число от 6 до 20
+        ('10', 'Так себе'),      # Число от 6 до 20
+        ('20', 'Так себе'),      # Число от 6 до 20
+        ('21', 'Отлично'),       # Число больше 20
+        ('100', 'Отлично'),      # Число больше 20
+        ('0', 'Неплохо'),        # 0 — четное и в диапазоне от 2 до 5
+    ]
+)
+def test_estimate_value(input_value, expected_output):
+    with patch('builtins.input', return_value=input_value), patch('builtins.print') as mock_print:
+        estimate_value(int(input_value))  # вызываем функцию
+        mock_print.assert_called_with(expected_output)
+
+
+
+
