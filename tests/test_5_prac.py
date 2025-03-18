754761acfb64a0bb028a91910c39068d671faef9
tsests <134438075+tsests@users.noreply.github.com>
2025-03-19 02:49:41 +0700

    Update test_5_prac.py

--- tests/test_5_prac.py
+++ tests/test_5_prac.py
@@ -20,10 +20,10 @@ from n_6 import define_winner
     [
         ("hello word of word", {'h': 1, 'e': 1, 'l': 2, 'o': 4, 'w': 2, 'r': 2, 'd': 2, 'f': 1}, {'hello': 1, 'word': 2, 'of': 1}),
         ("test test test", {'t': 6, 'e': 3, 's': 3}, {'test': 3}),
-        ("Python Python", {'p': 2, 'y': 2, 't': 2, 'h': 2, 'o': 2, 'n': 2}, {'python': 2}),
+        ("python python", {'p': 2, 'y': 2, 't': 2, 'h': 2, 'o': 2, 'n': 2}, {'python': 2}),
         ("", {}, {}),
         ("123 123 456", {'1': 2, '2': 2, '3': 2, '4': 1, '5': 1, '6': 1}, {'123': 2, '456': 1}),
-        ("A B C A", {'a': 2, 'b': 1, 'c': 1}, {'a': 2, 'b': 1, 'c': 1}),
+        ("a b c a", {'a': 2, 'b': 1, 'c': 1}, {'a': 2, 'b': 1, 'c': 1}),
     ],
 )
 def test_analyze_text(text, expected_chars, expected_words):
