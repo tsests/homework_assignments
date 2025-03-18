c5da9c731e26ccd3fcee67ace5db9b680d263b6a
tsests <134438075+tsests@users.noreply.github.com>
2025-03-19 02:57:53 +0700

    Update n_2.py

--- 5_prac/n_2.py
+++ 5_prac/n_2.py
@@ -2,9 +2,21 @@
 import ctypes
 
 def int_to_roman(number: int) -> str:
-    lib = ctypes.CDLL('./roman_converter.so')
-    lib.int_to_roman.restype = ctypes.c_char_p
-    return lib.int_to_roman(number).decode('utf-8')
+        roman_numerals = {
+        1000: "M", 900: "CM", 500: "D", 400: "CD",
+        100: "C", 90: "XC", 50: "L", 40: "XL",
+        10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"
+    }
+
+    result = ""
+    for value, symbol in roman_numerals.items():
+        while num >= value:
+            result += symbol
+            num -= value
+    return result
+    # lib = ctypes.CDLL('./roman_converter.so')
+    # lib.int_to_roman.restype = ctypes.c_char_p
+    # return lib.int_to_roman(number).decode('utf-8')
 
 def main():
     number = 13
