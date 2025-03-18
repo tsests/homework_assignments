8beabd5130fc5c22e2d1fda05aa7a10013a02787
tsests <134438075+tsests@users.noreply.github.com>
2025-03-19 02:44:09 +0700

    Update test_5_prac.py

--- tests/test_5_prac.py
+++ tests/test_5_prac.py
@@ -18,9 +18,9 @@ from n_6 import define_winner
 @pytest.mark.parametrize(
     "text, expected_chars, expected_words",
     [
-        ("hello, word of word", {'h': 1, 'e': 1, 'l': 2, 'o': 4, 'w': 2, 'r': 2, 'd': 2, 'f': 1}, {'hello': 1, 'word': 2, 'of': 1}),
+        ("hello word of word", {'h': 1, 'e': 1, 'l': 2, 'o': 4, 'w': 2, 'r': 2, 'd': 2, 'f': 1}, {'hello': 1, 'word': 2, 'of': 1}),
         ("test test test", {'t': 6, 'e': 3, 's': 3}, {'test': 3}),
-        ("Python! Python?", {'p': 2, 'y': 2, 't': 2, 'h': 2, 'o': 2, 'n': 2}, {'python': 2}),
+        ("Python Python", {'p': 2, 'y': 2, 't': 2, 'h': 2, 'o': 2, 'n': 2}, {'python': 2}),
         ("", {}, {}),
         ("123 123 456", {'1': 2, '2': 2, '3': 2, '4': 1, '5': 1, '6': 1}, {'123': 2, '456': 1}),
         ("A B C A", {'a': 2, 'b': 1, 'c': 1}, {'a': 2, 'b': 1, 'c': 1}),
